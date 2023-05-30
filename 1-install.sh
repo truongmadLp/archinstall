7bd7ae083047bc84b45d327948a4200463680927
truongmadlp <truongan.nguyen@outlook.de>
2023-05-30 23:21:59 +0200

    adding scripts

---
+++ 1-install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+clear
+echo "    _             _       ___           _        _ _ "
+echo "   / \   _ __ ___| |__   |_ _|_ __  ___| |_ __ _| | |"
+echo "  / _ \ | '__/ __| '_ \   | || '_ \/ __| __/ _' | | |"
+echo " / ___ \| | | (__| | | |  | || | | \__ \ || (_| | | |"
+echo "/_/   \_\_|  \___|_| |_| |___|_| |_|___/\__\__,_|_|_|"
+echo ""
+echo "by Truong An Nguyen (2023)"
+echo "-----------------------------------------------------"
+echo ""
+echo "Important: README instructions first!"
+echo "Warning: Run this script at your own risk."
+echo ""
+
+# ------------------------------------------------------
+# Enter partition names
+# ------------------------------------------------------
+lsblk
+read -p "Enter the name of the EFI partition (eg. sda1): " sda1
+read -p "Enter the name of the ROOT partition (eg. sda2): " sda2
+
+# ------------------------------------------------------
+# Sync time
+# ------------------------------------------------------
+timedatectl set-ntp true
+
+# ------------------------------------------------------
+# Format partitions
+# ------------------------------------------------------
+mkfs.fat -F 32 /dev/$sda1;
+mkfs.ext4 -f /dev/$sda2
+# mkfs.btrfs -f /dev/$sda2
+
+# ------------------------------------------------------
+# Mount points for btrfs
+# ------------------------------------------------------
+mount /dev/$sda2 /mnt
+#btrfs su cr /mnt/@
+#btrfs su cr /mnt/@cache
+#btrfs su cr /mnt/@home
+#btrfs su cr /mnt/@snapshots
+#btrfs su cr /mnt/@log
+#umount /mnt
+
+#mount -o compress=zstd:1,noatime,subvol=@ /dev/$sda2 /mnt
+#mkdir -p /mnt/{boot/efi,home,.snapshots,var/{cache,log}}
+#mount -o compress=zstd:1,noatime,subvol=@cache /dev/$sda2 /mnt/var/cache
+#mount -o compress=zstd:1,noatime,subvol=@home /dev/$sda2 /mnt/home
+#mount -o compress=zstd:1,noatime,subvol=@log /dev/$sda2 /mnt/var/log
+#mount -o compress=zstd:1,noatime,subvol=@snapshots /dev/$sda2 /mnt/.snapshots
+
+mkdir -p /mnt/boot/efi
+mount /dev/$sda1 /mnt/boot/efi
+
+# ------------------------------------------------------
+# Install base packages
+# ------------------------------------------------------
+pacstrap -K /mnt base base-devel git linux linux-firmware sof-firmware vim openssh reflector rsync amd-ucode intel-ucode nano vim
+
+# ------------------------------------------------------
+# Generate fstab
+# ------------------------------------------------------
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+
+# ------------------------------------------------------
+# Install configuration scripts
+# ------------------------------------------------------
+mkdir /mnt/archinstall
+cp 2-configuration.sh /mnt/archinstall/
+cp 3-yay.sh /mnt/archinstall/
+
+# ------------------------------------------------------
+# Chroot to installed system
+# ------------------------------------------------------
+arch-chroot /mnt ./archinstall/2-configuration.sh
+
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # archinstall
+
+# Load keyboard layout (replace de with us, fr, es if needed)
+loadkeys de-latin1
+
+# Increase font size (optional)
+setfont ter-p20b
+
+# Connect to WLAN (if not LAN)
+iwctl --passphrase [password] station wlan0 connect [network]
+
+# Check internet connection
+ping archlinux.org
+
+# Check partitions
+lsblk
+
+# Create partitions
+cfdisk /dev/sda
+
+# Partition 1: +512M ef00 (for EFI)
+# Partition 2: Available space 8300 (for Linux filesystem)
+# (Optional Partition 3 for Virtual Machines)
+# Swap lowkey useless
+# Write w, Confirm Y
+
+# Sync package
+pacman -Syy
+
+# Install git
+pacman -S git
+
+# Maybe it's required to install the current archlinux keyring
+# if the installation of git fails.
+pacman -S archlinux-keyring
+pacman -Syy
+
+# Clone Installation
+git clone https://github.com/truongmadLp/archinstall.git
+cd archinstall
+
+# Start the script
+./1-install.sh
